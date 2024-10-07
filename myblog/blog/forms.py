450c3ef1984f5ab086ac1deb61fc1d1a2f3880b8
tomerismeshitbai <tomerismesitbaj@gmail.com>
2024-10-07 18:41:56 +0500

    search and pagination

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -4,12 +4,11 @@ from .models import Post , Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','content','author')
+        fields = ('title','content')
         
         widgets = {
            'title': forms.TextInput(attrs = {'class': 'form-control'}),
-           'content': forms.Textarea(attrs = {'class': 'form-control'}),
-           'author': forms.Select(attrs = {'class': 'form-select'}),
+           'content': forms.Textarea(attrs = {'class': 'form-control'})
         }
             
 class CommentForm(forms.ModelForm):
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,11 +3,19 @@ from django.views.generic import ListView, DetailView , CreateView , UpdateView 
 from .models import Post , Comment
 from .forms import PostForm , CommentForm
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 class Home(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-created_at']
+    paginate_by = 5
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query)).distinct()
+        return Post.objects.all().order_by('-created_at')
     
 class PostDetail(DetailView):
     model = Post
@@ -17,6 +25,10 @@ class AddPost(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'post_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
 class EditPost(UpdateView):
     model = Post
